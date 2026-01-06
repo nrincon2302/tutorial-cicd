51fec21b012d4fd01626f82dcc2e43c6c2d046fc
nrincon2302 <n.rincon4@uniandes.edu.co>
2026-01-06 18:24:43 -0500

    Incorporate authentication for sign-up

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,48 @@
+import logger from '#config/logger';
+import { signUpSchema } from '#validations/auth.validation';
+import { formatValidationError } from '#utils/format';
+import { jwttoken } from '#utils/jwt';
+import { cookies } from '#utils/cookies';
+import { createUser } from '#services/auth.service';
+
+export const signUp = async (req, res, next) => {
+  try {
+    const validationResult = signUpSchema.safeParse(req.body);
+
+    if (!validationResult.success) {
+      return res.status(400).json({
+        error: 'Validation failed',
+        details: formatValidationError(validationResult.error),
+      });
+    }
+
+    const { name, email, password, role } = validationResult.data;
+    // Servicio de autenticación
+    const user = await createUser({ name, email, password, role });
+    const token = jwttoken.sign({
+      id: user.id,
+      email: user.email,
+      role: user.role,
+    });
+    cookies.set(res, 'token', token);
+
+    logger.info(`User registered successfully: ${email}`);
+    res.status(201).json({
+      message: 'User registered',
+      user: {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+      },
+    });
+  } catch (e) {
+    logger.error('Signup error', e);
+
+    if (e.message === 'User with this email already exists') {
+      return res.status(409).json({ error: 'Email already exists' });
+    }
+
+    next(e);
+  }
+};
---
+++ src/routes/auth.routes.js
@@ -0,0 +1,16 @@
+import { signUp } from '#controllers/auth.controller';
+import express from 'express';
+
+const router = express.Router();
+
+router.post('/sign-up', signUp);
+
+router.post('/sign-in', (req, res) => {
+  res.send('POST /api/auth/sign-in response');
+});
+
+router.post('/sign-out', (req, res) => {
+  res.send('POST /api/auth/sign-out response');
+});
+
+export default router;
