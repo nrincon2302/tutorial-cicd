53bada258c0a981136040bf532f8e3734791a2c9
nrincon2302 <n.rincon4@uniandes.edu.co>
2026-01-07 00:33:05 -0500

    Implement coverage and basic unit tests

--- src/app.js
+++ src/app.js
@@ -43,4 +43,8 @@ app.get('/api', (req, res) => {
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
 
+app.use((req, res) => {
+  res.status(404).json({ error: 'Route not found' });
+});
+
 export default app;
